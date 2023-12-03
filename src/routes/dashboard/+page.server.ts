cbbc4ac7f4775fb7b68079aabbd43d12f7a50638
son.tran <son.tran@team.enouvo.com>
2023-12-03 22:55:10 +0700

    feat(master): update crud

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,4 +1,3 @@
-import { getCryptoCurrencies } from "$lib/api/crypto-currencies";
 import { getNuplIndex, getSupplyInProfitIndex } from "$lib/api/crypto-quant.js";
 import { getFearAndGreedIndex } from "$lib/api/feat-and-greed";
 import prisma from "$lib/db/prisma";
@@ -9,37 +8,20 @@ export async function load(event) {
     throw redirect(302, "/sign-in");
   }
 
-  const [
-    currencies,
-    coinsObj,
-    fearAndGreedIndex,
-    supplyInProfitIndex,
-    nuplIndex,
-  ] = await Promise.all([
-    prisma.currency.findMany({
-      where: {
-        userId: event.locals.session.userId,
-      },
-    }),
-    getCryptoCurrencies(),
-    getFearAndGreedIndex(),
-    getSupplyInProfitIndex(),
-    getNuplIndex(),
-  ]);
-
-  const formattedCurrencies = currencies.map((cur) => {
-    const coin = coinsObj?.[cur.symbol];
-    const value = Number(cur.amount) * coin.price;
-    return {
-      ...coin,
-      url: cur.url,
-      amount: Number(cur.amount),
-      value: value,
-    };
-  });
+  const [currencies, fearAndGreedIndex, supplyInProfitIndex, nuplIndex] =
+    await Promise.all([
+      prisma.currency.findMany({
+        where: {
+          userId: event.locals.session.userId,
+        },
+      }),
+      getFearAndGreedIndex(),
+      getSupplyInProfitIndex(),
+      getNuplIndex(),
+    ]);
 
   return {
-    currencies: formattedCurrencies,
+    currencies: currencies,
     fearAndGreedIndex,
     supplyInProfitIndex,
     nuplIndex,
