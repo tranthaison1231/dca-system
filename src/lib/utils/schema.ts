470a3de6eb6742e95140a41848914e6f36602aad
son.tran <son.tran@team.enouvo.com>
2023-12-04 11:29:11 +0700

    feat(master): update supply in profit

--- src/lib/utils/schema.ts
+++ src/lib/utils/schema.ts
@@ -1,12 +1,12 @@
 import * as z from "zod";
 
 export const updateCurrencySchema = z.object({
-  amount: z.coerce.number(),
+  amount: z.coerce.number().min(0),
 });
 
 export const createCurrencySchema = z.object({
   symbol: z.string(),
-  amount: z.coerce.number(),
+  amount: z.coerce.number().min(0),
   url: z.string(),
   name: z.string().optional(),
 });
