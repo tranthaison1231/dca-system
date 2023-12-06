1a9935e3a9cbb70c9d9ab18f0f0850689ebed0dd
son.tran <son.tran@team.enouvo.com>
2023-12-06 17:23:54 +0700

    feat(master): add webhook user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,56 +3,12 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // const data = await prisma.currency.createMany({
-  //   data: [
-  //     {
-  //       symbol: "BTC",
-  //       amount: "0.10971092",
-  //       userId: "user_2YwM6Lp6t3ETj4k6aB2ZedU56fm",
-  //     },
-  //     {
-  //       symbol: "ETH",
-  //       amount: "0.59430315",
-  //       userId: "user_2YwM6Lp6t3ETj4k6aB2ZedU56fm",
-  //     },
-  //     {
-  //       symbol: "USDT",
-  //       amount: "392",
-  //       userId: "user_2YwM6Lp6t3ETj4k6aB2ZedU56fm",
-  //     },
-  //     {
-  //       symbol: "BNB",
-  //       amount: "0.8156",
-  //       userId: "user_2YwM6Lp6t3ETj4k6aB2ZedU56fm",
-  //     },
-  //     {
-  //       symbol: "XRP",
-  //       amount: "496",
-  //       userId: "user_2YwM6Lp6t3ETj4k6aB2ZedU56fm",
-  //     },
-  //     {
-  //       symbol: "SOL",
-  //       amount: "0",
-  //       userId: "user_2YwM6Lp6t3ETj4k6aB2ZedU56fm",
-  //     },
-  //     {
-  //       symbol: "ADA",
-  //       amount: "3046",
-  //       userId: "user_2YwM6Lp6t3ETj4k6aB2ZedU56fm",
-  //     },
-  //     {
-  //       symbol: "DOGE",
-  //       amount: "0",
-  //       userId: "user_2YwM6Lp6t3ETj4k6aB2ZedU56fm",
-  //     },
-  //     {
-  //       symbol: "TRX",
-  //       amount: "0",
-  //       userId: "user_2YwM6Lp6t3ETj4k6aB2ZedU56fm",
-  //     },
-  //   ],
-  // });
-  // const data = await prisma.currency.findMany({});
+  const data = await prisma.currency.updateMany({
+    data: {
+      updatedAt: new Date(),
+    },
+  });
+  console.log(data);
 }
 
 main()
---
+++ src/routes/api/webhooks/user/+server.ts
@@ -0,0 +1,59 @@
+import { WEBHOOK_SECRET } from "$env/static/private";
+import prisma from "$lib/db/prisma";
+import { error } from "@sveltejs/kit";
+import { Webhook, type WebhookRequiredHeaders } from "svix";
+import type { RequestEvent } from "./$types";
+
+type EventType = "user.created" | "user.updated" | "*";
+
+type Event = {
+  data: Record<string, string | number>;
+  object: "event";
+  type: EventType;
+};
+
+async function handler(event: RequestEvent) {
+  const payload = await event.request.json();
+  const headersList = event.request.headers;
+  const heads = {
+    "svix-id": headersList.get("svix-id"),
+    "svix-timestamp": headersList.get("svix-timestamp"),
+    "svix-signature": headersList.get("svix-signature"),
+  };
+  const wh = new Webhook(WEBHOOK_SECRET);
+  let evt: Event | null = null;
+
+  try {
+    evt = wh.verify(
+      JSON.stringify(payload),
+      heads as WebhookRequiredHeaders
+    ) as Event;
+  } catch (err) {
+    throw error(400, {
+      message: "Invalid signature",
+    });
+  }
+
+  const eventType: EventType = evt.type;
+  if (eventType === "user.created" || eventType === "user.updated") {
+    const { id, ...attributes } = evt.data;
+
+    if (typeof id !== "string") {
+      throw error(400, {
+        message: "ID must be a string",
+      });
+    }
+    await prisma.user.upsert({
+      where: { clerkId: id },
+      create: {
+        clerkId: id,
+        attributes,
+      },
+      update: { attributes },
+    });
+  }
+}
+
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
