cf3a3a48d4b6b61dad9dda17734bd9d17cd32c8c
son.tran <son.tran@team.enouvo.com>
2023-12-19 07:38:20 +0700

    feat(master): fix api get suuply in profit

--- src/routes/api/index/supply-in-profit/+server.ts
+++ src/routes/api/index/supply-in-profit/+server.ts
@@ -2,15 +2,13 @@ import { json } from "@sveltejs/kit";
 import dayjs from "dayjs";
 
 export const GET = async () => {
-  const mrd = dayjs().subtract(1, "day").startOf("day").unix();
+  const mrd = dayjs().subtract(5, "day").startOf("day").unix();
 
-  console.log(mrd);
   const res = await fetch(
     `https://www.mesmerdata.com/n2/mesmer_charts.php?q=79&mrd=${mrd}`,
   );
   const data = await res.json();
 
-  console.log("data", data);
   return json({
     status: "success",
     value: data[data.length - 1][1],
